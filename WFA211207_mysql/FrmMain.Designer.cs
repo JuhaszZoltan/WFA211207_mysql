f684cd60c05283d27eb11587ea7fb3a72d9e715e
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2021-12-07 17:10:17 +0100

    Add project files.

---
+++ WFA211207_mysql/FrmMain.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WFA211207_mysql
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.rtb.Location = new System.Drawing.Point(107, 96);
+            this.rtb.Name = "richTextBox1";
+            this.rtb.Size = new System.Drawing.Size(237, 225);
+            this.rtb.TabIndex = 0;
+            this.rtb.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 455);
+            this.Controls.Add(this.rtb);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb;
+    }
+}
+
---
+++ WFA211207_mysql/FrmMain.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WFA211207_mysql
+{
+    public partial class FrmMain : Form
+    {
+        public string ConnectionString { get; set; }
+        public FrmMain()
+        {
+            ConnectionString =
+                "Server = winsql.verebely.dc; " +
+                "Database = diak6; " +
+                "Uid = diak6; " +
+                "Pwd = jI50aR;";
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (var c = new MySqlConnection(ConnectionString))
+            {
+                c.Open();
+                var command = new MySqlCommand("SELECT * FROM tabla;", c);
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    rtb.Text += $"{reader.GetString(1)}\n";
+                }
+                reader.Close();
+                c.Close();  
+            }
+        }
+    }
+}
